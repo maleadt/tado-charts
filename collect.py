4b7bd2af9518ddf2a27c9e1062b6636c5b2916cb
Tim Besard <tim.besard@gmail.com>
2017-03-31 13:19:27 +0200

    Create the table when collecting.

--- collect.py
+++ collect.py
@@ -5,6 +5,7 @@ import urllib.request
 import urllib.parse
 import urllib.error
 import pymysql
+import warnings
 
 import private
 
@@ -150,6 +151,25 @@ for name in private.zones:
     zone = home.getZone(name=name)
     state = zone.getState()
 
+    # create new table
+    with warnings.catch_warnings(), mysql.cursor() as cursor:
+        # temperatures are < 100 with up to two digits past the comma --> DECIMAL(4,2)
+        # percentages are <= 100 with only one digit past the comma --> DECIMAL(4,1)
+        sql = """
+            CREATE TABLE IF NOT EXISTS `{}` (
+                `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                `outsideTemperature` DECIMAL(4,2),
+                `setpoint` DECIMAL(4,2),
+                `temperature` DECIMAL(4,2),
+                `humidity` DECIMAL(4,1),
+                `heatingpower` DECIMAL(4,1),
+                PRIMARY KEY (`timestamp`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+            AUTO_INCREMENT=1 ;
+        """.format(name)
+        warnings.filterwarnings('ignore', ".*Table '{}' already exists.*".format(name))
+        cursor.execute(sql)
+
     heatingpower = state["activityDataPoints"]["heatingPower"]["percentage"]
 
     temperature = state["sensorDataPoints"]["insideTemperature"]["celsius"]
@@ -159,6 +179,7 @@ for name in private.zones:
     setting = overlay["setting"] if overlay else state["setting"]
     setpoint = setting["temperature"]["celsius"] if setting["power"] == 'ON' else None
 
+    # insert data
     with mysql.cursor() as cursor:
         sql = """
             INSERT INTO `{}`
