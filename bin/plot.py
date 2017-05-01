211ee809d0030b2ab822f23d95b1bc15c82c1cf1
Tim Besard <tim.besard@gmail.com>
2017-05-01 15:50:00 +0200

    Tweak visuals.

--- bin/plot.py
+++ bin/plot.py
@@ -4,46 +4,52 @@ import os
 import sys
 import datetime
 import pymysql
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
 import matplotlib.patches as mpatches
+from scipy.signal import savgol_filter
 
 import private
+
+
+def smooth(y):
+    return savgol_filter(y, 41, 1)
+
 
 def plot(zone, timestamps, values, time_lower, time_upper, name):
     outsideTemperature, setpoint, temperature, humidity, heatingpower = values
 
     # mask heatingpower==0 for clarity
-    heatingpower = numpy.ma.masked_where(heatingpower == 0, heatingpower)
+    heatingpower = np.ma.masked_where(heatingpower == 0, heatingpower)
 
 
     # initialize
 
-    fig, ax1 = plt.subplots(figsize=(15,7))
+    fig, ax1 = plt.subplots(figsize=(12,5))
 
 
     # temperatures
 
     ax1.set_xlim([time_lower, time_upper])
     ax1.xaxis.set_major_locator(mdates.HourLocator())
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
 
     ax1.set_ylabel('temperature (°celsius)')
     ax1.yaxis.set_major_locator(ticker.MultipleLocator(1))
     ax1.yaxis.grid(alpha=0.25)
 
-    ax1.plot(timestamps, outsideTemperature, label="Outside",
-             linestyle="solid", color="darkgreen")
+    ax1.plot(timestamps, smooth(outsideTemperature), label="Outside",
+             linestyle="solid", color="gray")
 
-    ax1.plot(timestamps, setpoint, label="Requested",
-             linestyle="solid", color="darkorange")
+    ax1.plot(timestamps, setpoint, label="Setpoint",
+             linestyle="solid", color="lime", alpha=.5)
 
-    ax1.plot(timestamps, temperature, label="Measured",
-             linestyle="solid", color="red")
+    ax1.plot(timestamps, smooth(temperature), label="Measured",
+             linestyle="solid", color="orangered")
 
-    ax1.legend(loc='lower left', bbox_to_anchor=(0, -0.11, 1, 0), ncol=3,
+    ax1.legend(loc='lower left', bbox_to_anchor=(0, -0.175, 1, 0), ncol=3,
                fancybox=True, shadow=True)
 
 
@@ -52,20 +58,18 @@ def plot(zone, timestamps, values, time_lower, time_upper, name):
     ax2 = ax1.twinx()
 
     ax2.set_xlim([time_lower, time_upper])
-    ax2.xaxis.set_major_locator(mdates.HourLocator())
-    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
     ax2.set_ylabel('percentage')
     ax2.set_ylim([0,100])
     ax2.yaxis.set_major_locator(ticker.MultipleLocator(10))
 
-    p1 = ax2.plot(timestamps, humidity, label="Humidity",
-                  linestyle="solid", color="blue", alpha=.1,)
-    ax2.fill_between(timestamps, 0, humidity, color='b', alpha=.1)
+    p1 = ax2.plot(timestamps, smooth(humidity), label="Humidity",
+                  linestyle="solid", color="darkcyan", alpha=.1,)
+    ax2.fill_between(timestamps, 0, smooth(humidity), color='darkcyan', alpha=.1)
 
     p2 = ax2.plot(timestamps, heatingpower, label="Heating power",
-                  linestyle="solid", color="red", alpha=.15)
-    ax2.fill_between(timestamps, 0, heatingpower, color='r', alpha=.15)
+                  linestyle="solid", color="orange", alpha=.15)
+    ax2.fill_between(timestamps, 0, heatingpower, color='orange', alpha=.15)
 
     # the fill_between can't be represented directly in the legend,
     # so create proxy agents which don't live in the plot
@@ -73,7 +77,7 @@ def plot(zone, timestamps, values, time_lower, time_upper, name):
     p2_proxy = mpatches.Patch(color=p2[0].get_color(), alpha=p2[0].get_alpha(), label=p2[0].get_label())
 
     ax2.legend(handles=[p1_proxy, p2_proxy],
-               loc='lower right', bbox_to_anchor=(0, -0.11, 1, 0), ncol=2,
+               loc='lower right', bbox_to_anchor=(0, -0.175, 1, 0), ncol=2,
                fancybox=True, shadow=True)
 
 
@@ -86,7 +90,7 @@ def plot(zone, timestamps, values, time_lower, time_upper, name):
                  horizontalalignment='right', verticalalignment='upper')
 
     plt.tight_layout()
-    fig.subplots_adjust(bottom=0.1)
+    fig.subplots_adjust(bottom=0.15)
 
     dirname = os.path.dirname(sys.argv[0])
     plt.savefig("{}/{}.png".format(dirname, name))
@@ -112,14 +116,14 @@ for zone in private.zones:
         cursor.execute(sql, (time_lower, time_upper))
         data = cursor.fetchall()
 
-        timestamps = numpy.asarray(list(map(lambda vals: vals[0], data)))
+        timestamps = np.asarray(list(map(lambda vals: vals[0], data)))
         def convert(val):
             if val is None:
                 # we can't create an array with None, or operations like 'isfinite' break
                 return float('NaN')
             else:
                 return float(val)
-        values = numpy.asarray(list(map(lambda vals: tuple(map(convert, vals[1:])), data))).T
+        values = np.asarray(list(map(lambda vals: tuple(map(convert, vals[1:])), data))).T
 
         # plot daily chart
         plot(zone, timestamps, values, time_lower, time_upper,
